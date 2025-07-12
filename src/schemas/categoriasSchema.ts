3f3d0547d5a7b70a24e0babf791b6f113bd29d8f
Dennis20DC <dc8042613@gmail.com>
2025-07-11 20:22:48 -0500

    actualizado1

--- src/schemas/categoriasSchema.ts
+++ src/schemas/categoriasSchema.ts
@@ -5,5 +5,9 @@ export const categoriasCrearSchema = Joi.object({
       'any.required': 'El nombre del cliente es obligatorio.',
       'string.base': 'El nombre debe ser un texto.',
       'string.max': 'El nombre no debe exceder los 100 caracteres.'
-    })
+    }),
+    descripcion: Joi.string().max(255).optional().allow('', null).messages({
+    'string.base': 'La descripción debe ser un texto.',
+    'string.max': 'La descripción no debe exceder los 255 caracteres.'
+  })
 });
